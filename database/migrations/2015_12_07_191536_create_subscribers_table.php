e3a452dea8f3501cd1b826ddf3b46bddc50c0122
Agustin Camino <agustin.camino@gmail.com>
2015-12-07 14:38:22 -0500

    Add migration to create subscribers

---
+++ database/migrations/2015_12_07_191536_create_subscribers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscribersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('phone_number')->unique();
+            $table->boolean('subscribed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('subscribers');
+    }
+}
