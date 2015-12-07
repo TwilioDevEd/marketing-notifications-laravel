e128e56764d2ae2f4e373bf4bca1f10df96dd286
Agustin Camino <agustin.camino@gmail.com>
2015-12-07 16:55:09 -0500

    Add Twilio service provider

---
+++ app/Providers/TwilioServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Service_Twilio;
+
+class TwilioServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            'Services_Twilio', function ($app) {
+                $accountSid = config('services.twilio')['accountSid'];
+                $authToken  = config('services.twilio')['authToken'];
+
+                return new Services_Twilio($accountSid, $authToken);
+            }
+        );
+    }
+}
