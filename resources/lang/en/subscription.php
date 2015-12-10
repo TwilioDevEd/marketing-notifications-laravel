51a63b3046d7b1d70d878484ef36406a0da93c7a
Agustin Camino <agustin.camino@gmail.com>
2015-12-09 19:49:49 -0500

    Fix typos on unsubscribed confirmation message

--- resources/lang/en/subscription.php
+++ resources/lang/en/subscription.php
@@ -5,6 +5,6 @@ return [
     'thanks'                    => 'Thanks for contacting TWBC! Text \'subscribe\' if you would to receive updates via text message.',
     'unavailable_command'       => 'Sorry, we don\'t recognize that command. Available commands are: \'subscribe\' or \'unsubscribe\'.',
     'subscribed_confirmation'   => 'You are now subscribed for updates',
-    'unsubscribed_confirmation' => 'You have unsubscribed from notifications. Test \'subscribe\' to start receieving updates again',
+    'unsubscribed_confirmation' => 'You have unsubscribed from notifications. Text \'subscribe\' to start receiving updates again',
 
 ];
