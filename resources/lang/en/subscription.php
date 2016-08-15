f92c4b2cf6c0fa1cb1cbd314bacf66983c533cc7
Mario Celi <mceli@stackbuilders.com>
2016-08-15 17:14:44 -0500

    Fix SMS commands

--- resources/lang/en/subscription.php
+++ resources/lang/en/subscription.php
@@ -2,9 +2,9 @@
 
 return [
 
-    'thanks'                    => 'Thanks for contacting TWBC! Text \'subscribe\' if you would to receive updates via text message.',
-    'unavailable_command'       => 'Sorry, we don\'t recognize that command. Available commands are: \'subscribe\' or \'unsubscribe\'.',
+    'thanks'                    => 'Thanks for contacting TWBC! Text \'add\' if you would to receive updates via text message.',
+    'unavailable_command'       => 'Sorry, we don\'t recognize that command. Available commands are: \'add\' or \'remove\'.',
     'subscribed_confirmation'   => 'You are now subscribed for updates',
-    'unsubscribed_confirmation' => 'You have unsubscribed from notifications. Text \'subscribe\' to start receiving updates again',
+    'unsubscribed_confirmation' => 'You have unsubscribed from notifications. Text \'add\' to start receiving updates again',
 
 ];
