ff48b6610965d13e5a814fd292df3bfacd59ef11
Paul Kamp <pkamp@twilio.com>
2018-02-02 18:23:24 -0800

    Semis

--- app/Http/Controllers/SubscribersController.php
+++ app/Http/Controllers/SubscribersController.php
@@ -72,15 +72,15 @@ class SubscribersController extends Controller
     private function messageText($messageType = 'unknown') {
         switch($messageType) {
             case 'add':
-                return "Thank you for subscribing to notifications!"
+                return "Thank you for subscribing to notifications!";
                 break;
             case 'remove':
                 return "The number you texted from will no longer receive notifications. 
-            To start receiving notifications again, please text 'add'."
+            To start receiving notifications again, please text 'add'.";
                 break;
             default:
                 return "I'm sorry, that's not an option I recognize. 
-            Please, let me know if I should 'add' or 'remove' this number from notifications."
+            Please, let me know if I should 'add' or 'remove' this number from notifications.";
                 break;
         }
     }
